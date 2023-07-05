3b8c1fdd0cb25de51c063075687f4d9bf657250f
Foysal Hossain <foysalhossain772@gmail.com>
2023-07-05 17:11:16 +0600

    banner section added

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header>
+    <header className='py-8'>
       <div className='container mx-auto'>
         <div className='flex justify-between items-center'>
           {/* logo */}
           <a href="#">
-            <h1 className='text-3xl font-bold'><span className='text-purple-600'>Foysal</span> Hossain</h1>
+            <h1 className='text-3xl font-bold'><span className='text-accent'>Foysal</span> Hossain</h1>
           </a>
           {/* button */}
           <button className='btn btn-sm'>Work with me</button>
