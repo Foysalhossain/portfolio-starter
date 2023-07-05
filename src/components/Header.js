746e16b886839ba4393f9f31fa1c76d1da2cbc0e
Foysal Hossain <foysalhossain772@gmail.com>
2023-07-05 15:45:02 +0600

    header section added

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className='container mx-auto'>
+        <div className='flex justify-between items-center'>
+          {/* logo */}
+          <a href="#">
+            <h1 className='text-3xl font-bold'><span className='text-purple-600'>Foysal</span> Hossain</h1>
+          </a>
+          {/* button */}
+          <button className='btn btn-sm'>Work with me</button>
+        </div>
+      </div>
+    </header>
+
   )
 };
 
